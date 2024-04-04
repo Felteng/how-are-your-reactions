f8e5a67bd0f78302b783bb0eb42d1e222ca8b1c2
Felix Tenggren <felixtenggren1@gmail.com>
2024-04-04 15:37:46 +0000

    Nest timer in reactionGame, add repeat w/o refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,63 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let gameIsOn = false;
+document.addEventListener("DOMContentLoaded", startGame())
 
-    const startBox = document.getElementById("reaction-box");
-    startBox.addEventListener("click", function() {
 
-        if (gameIsOn === false) {
-            gameIsOn = true;
-            startBox.style.backgroundColor = "red";
-            gameStartTimer();
-        } else if (gameIsOn === true) {
-            null
-        }
-    });
-})
+function startGame() {
+let gameIsOn = false;
+
+const startBox = document.getElementById("reaction-box");
+startBox.addEventListener("click", function() {
+
+    if (gameIsOn === false) {
+        gameIsOn = true;
+        document.getElementsByClassName("user-time")[0].style.display = "none";
+        startBox.style.backgroundColor = "red";
+        gameStartTimer();
+    } else if (gameIsOn === true) {
+        null
+    }
+});
+}
 
 function gameStartTimer() {
+
         const min = 1;
         const max = 5;
         let startTime = Math.floor(Math.random() * (max - min + 1) + min); //Generate a random start time between 1 - 5 seconds
-        setTimeout(startGame, startTime * 1000);
-};
+        setTimeout(reactionGame, startTime * 1000);
 
-function startGame() {
-    const startBox = document.getElementById("reaction-box");
-    startBox.style.backgroundColor = "green";
-    speedTimer() 
-}
+};
 
 function reactionGame() {
 
-}
-
-function speedTimer() {
+    const startBox = document.getElementById("reaction-box");
+    startBox.style.backgroundColor = "green";
     let sec = 0;
 
+   /**
+    * Timer to count the time it takes the user to click once
+    * the game has begun
+    */
     let timer = setInterval(function() {
-        sec += 1;
+       sec++
+       console.log(sec)
     } ,0);
 
     /**
-     * Wait for user to react and click the reaction-box
-     * to end the timer and display the time it took
-     */
-    document.getElementById("reaction-box").addEventListener("click", function() {
-        console.log(`Your time was: ${sec.toFixed(3)*4}`) 
+    * Wait for user to react and click the reaction-box
+    * to end the timer and display the time it took
+    */
+    startBox.addEventListener("click", stopGame)
+
+    function stopGame() {
+
+        console.log(`Your time was: ${sec.toFixed(3)*4}`);
         clearInterval(timer);        
         document.getElementsByClassName("user-time")[0].style.display = "block";
-        document.getElementById("time-score").innerHTML = sec.toFixed(3) * 4 // Multiply by 4 here to account for the standard 4ms interval delay in browsers
-    })
+        document.getElementById("time-score").innerHTML = sec.toFixed(3) * 4; // Multiply by 4 here to account for the standard 4ms interval delay in browsers
+        startBox.removeEventListener("click", stopGame)        
+        startGame()
+
+    }
 }
 
 function precisionGame() {
