a6257145a323dcfe18baa04a7ab654adf209045d
Felix Tenggren <felixtenggren1@gmail.com>
2024-04-05 10:23:17 +0000

    Add function to start timer to prevent cheating

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,44 @@ startBox.addEventListener("click", function() {
 
     if (gameIsOn === false) {
         gameIsOn = true;
-        document.getElementsByClassName("user-time")[0].style.display = "none";
+        document.getElementById("notify-user").style.display = "none";
         startBox.style.backgroundColor = "red";
         gameStartTimer();
     } else if (gameIsOn === true) {
-        null
+        null // Make it so multiple games cannot get started at the same time by clicking lots.
     }
 });
 }
 
 function gameStartTimer() {
 
-        const min = 1;
-        const max = 5;
-        let startTime = Math.floor(Math.random() * (max - min + 1) + min); //Generate a random start time between 1 - 5 seconds
-        setTimeout(reactionGame, startTime * 1000);
+    const min = 1;
+    const max = 5;
+    let startTime = Math.floor(Math.random() * (max - min + 1) + min); // Generate a random start time between 1 - 5 seconds
+    const startTimer = setTimeout(reactionGame, startTime * 1000);
 
+    const startBox = document.getElementById("reaction-box");
+    const userNotification = document.getElementById("notify-user");
+
+    /**
+     * End the game start timer early if the user
+     * attempts to make preemptively click before
+     * the game has started
+     */
+    startBox.addEventListener("click", tooSoon); // Check if user clicks during start timer
+
+    setTimeout(function() {
+        startBox.removeEventListener("click", tooSoon) // Remove listener at the same timer as the game start timer to prevent bugs
+    }, startTime * 1000)
+
+    function tooSoon() {
+
+            clearTimeout(startTimer);
+            userNotification.textContent = "Oops! Too soon.. Try again!";
+            userNotification.style.display = "block";
+            startBox.removeEventListener("click", tooSoon)
+            startGame()
+    }  
 };
 
 function reactionGame() {
@@ -39,7 +61,6 @@ function reactionGame() {
     */
     let timer = setInterval(function() {
        sec++
-       console.log(sec)
     } ,0);
 
     /**
@@ -52,9 +73,9 @@ function reactionGame() {
 
         console.log(`Your time was: ${sec.toFixed(3)*4}`);
         clearInterval(timer);        
-        document.getElementsByClassName("user-time")[0].style.display = "block";
-        document.getElementById("time-score").innerHTML = sec.toFixed(3) * 4; // Multiply by 4 here to account for the standard 4ms interval delay in browsers
-        startBox.removeEventListener("click", stopGame)        
+        document.getElementById("notify-user").style.display = "block";
+        document.getElementById("notify-user").innerHTML = `Your time was: ${sec.toFixed(3) * 4}ms` // Multiply by 4 here to account for the standard 4ms interval delay in browsers
+        startBox.removeEventListener("click", stopGame) // Remove eventListener to avoid the game function lingering and restarting multiple games every click.
         startGame()
 
     }
