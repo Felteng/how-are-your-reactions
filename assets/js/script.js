2fe5546981254d14d5a7ad8fcc1c1ebbf77947b2
Felix Tenggren <felixtenggren1@gmail.com>
2024-04-16 12:51:21 +0000

    Add 2 missing semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ let gridGameIsOn = false;
 let gridTile = null;
 let gridGameScore = document.getElementById("score");
 let gridGameHighscore = document.getElementById("highscore");
-let gridHighscore = localStorage.getItem("gridHighscore")
+let gridHighscore = localStorage.getItem("gridHighscore");
 
 
 
@@ -266,7 +266,7 @@ function gridGameTime() {
 function stopGridGame() {
     if (gridScore > gridHighscore) { // Updates highscore if possible
         localStorage.setItem("gridHighscore", gridScore);
-        let gridHighscore = localStorage.getItem("gridHighscore")
+        let gridHighscore = localStorage.getItem("gridHighscore");
         gridGameHighscore.innerHTML = gridHighscore;
 
     }
